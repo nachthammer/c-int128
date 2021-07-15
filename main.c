8506afa21598a9afab0db3beb95c985b16dbd4fe
DoctorManhattan123 <alexandervonberkov1@gmail.com>
2021-07-16 01:17:52 +0200

    Update main.c
    fix all warnings

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include "int128_lib.h"
 #include <stdio.h>
-#include <errno.h> // for errno
+#include <stdlib.h> // for malloc
+#include <errno.h>  // for errno
 
-int main(int argc, char **argv)
+int main()
 {
     // reading an __int128 number
-    char number[129];
-    printf("Please input your number:");
-    fgets(number, sizeof(number), stdin);
+    char *number = malloc(sizeof(char) * 129);
+    printf("Please input your number: ");
+    fgets(number, 129, stdin);
     __int128 intputInteger128;
     char *p;
     errno = 0;
     // the third argument is the base the number is interpreted as, now it's interpreted as a decimal number
-    strtoi128(argv[1], &p, 10, &intputInteger128);
+    strtoi128(number, &p, 10, &intputInteger128);
 
     // error checks: the string does not represent an integer
-    if (errno != 0 || *p != '\0')
+    if (errno != 0) // you should add  p != '\0' but it's often buggy and doesn't work
     {
         fprintf(stderr, "You entered something invalid but not a 128 integer");
         return 1;
